673aa8850caf03a081b35e313e18aa0d63a4adbe
Thomas Richter <t.richter@intershop.de>
2022-04-04 12:34:40 +0200

    task8 completed

--- src/app/extensions/inventory/pages/warehouses/warehouses-page.component.ts
+++ src/app/extensions/inventory/pages/warehouses/warehouses-page.component.ts
@@ -1,8 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { WarehousesFacade } from '../../facades/warehouses.facade';
+import { Warehouse } from '../../models/warehouse/warehouse.model';
 
 @Component({
   selector: 'ish-warehouses-page',
   templateUrl: './warehouses-page.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class WarehousesPageComponent {}
+export class WarehousesPageComponent implements OnInit{
+warehouses$: Observable<Warehouse[]>;
+constructor(private warehousesFacade: WarehousesFacade){}
+
+ngOnInit() {
+  this.warehouses$ = this.warehousesFacade.getWarehouses$;
+
+}
+
+}
